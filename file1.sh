04ec066c10d88ae225649388a43c9bc4909a97fb
efratbatzon <efratb4155@gmail.com>
2023-08-08 13:14:30 +0300

    commit1

---
+++ file1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Not enough arguments received"
+  exit 1
+fi
+
+file="$1"
+if [ ! -f "$file" ]; then
+ echo "the file not exsist"
+ exit 1
+fi
+
+separator="$2"
+
+# Set IFS (Internal Field Separator) to the specified separator
+IFS="$separator"
+
+# Iterate through each line in the file
+while read -r line; do
+  fields=($line)  
+  num_fields=${#fields[@]}  
+
+  # Print each field with its index
+  for ((i = 0; i < num_fields; i++)); do
+    echo "field $((i + 1)): ${fields[i]}"
+  done
+done < "$file"
+
+# Reset IFS to its default value (space, tab, newline)
+IFS=$' \t\n'
